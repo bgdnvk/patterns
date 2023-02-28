031ab38d7c38875360421a2013adafe8327204c6
bgdnvk <bnovykov@gmail.com>
2023-02-28 22:03:15 +0100

    docs

--- factory/ts-factory.ts
+++ factory/ts-factory.ts
@@ -1,3 +1,4 @@
+//pizza is going to be the product that a factory can make
 interface Pizza {
     name: string
     getName(): string
@@ -5,6 +6,7 @@ interface Pizza {
     eat(): void
 }
 
+//this is just an example, NY pizza is different from Chicago pizza
 class NewYorkPizza implements Pizza {
 
     name: string
@@ -45,10 +47,12 @@ class ChicagoPizza implements Pizza {
     }
 }
 
+//this is our creator, we can use this interfece anywhere in order to make our products(pizzas)
 interface PizzaFactory {
     makePizza(pizza: Pizza): Pizza
 }
 
+//lets think of different restaurants that want to make pizzas
 class NewYorkRestaurant implements PizzaFactory {
 
     makePizza(): Pizza {
@@ -63,12 +67,19 @@ class ChicagoRestaurant implements PizzaFactory {
     }
 }
 
+//create our factories
 const newYorkRestaurant: NewYorkRestaurant = new NewYorkRestaurant()
 const chicagoRestaurant: ChicagoRestaurant = new ChicagoRestaurant() 
 
+//get the product
 const myNewYorkPizza = newYorkRestaurant.makePizza()
 const myChicagoPizza = chicagoRestaurant.makePizza()
 
+//here we use our products
+//this is a simple example but remember you can add a lot more logic and extend the examples
+//the point of the pattern is so that you could use the factories anywhere you need
+//and modify the logic inside
+//try adding a delivery method for example to one of the restaurants or something concrete to the pizzas
 const newYorkPizzaName = myNewYorkPizza.getName()
 console.log(`NY pizza name is: ${newYorkPizzaName}`)
 myNewYorkPizza.cookingTime()
